14414d4280b3cb39f90c4b1e4334c1cea96322f8
Jadon Orr <jadonorr01@gmail.com>
2024-02-10 18:34:25 -0500

    use button to send request

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import { API_URL } from 'react-native-dotenv';
 
 export default function App() {
-  fetch(`${API_URL}/`)
-      .then(res => res.json())
-      .then(json => console.log(json))
+  const sendReq = () => {
+    console.log(`Sending GET request to '${API_URL}/'...`);
+    return fetch(`${API_URL}/`)
+      .then(res => res.text())
+      .then(jsonText => console.log(`RES: ${jsonText}`))
       .catch(err => console.error(err));
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={sendReq} style={styles.button}>
+        <Text style={styles.buttonText}>Send Request</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#5db2ef',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#fafafa',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
